9c8d9e035e3bddb75ac3df661486f25f08d1fe7d
Optifun <frdima19@gmail.com>
2021-06-25 16:22:41 +0700

    Update entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-function init(firstName: string, lastName: string) {
-  return `${firstName} ${lastName}`;
-}
+import net from "net";
+import dgram from "dgram";
+import { decode, encode } from "@msgpack/msgpack";
+import { UDPServer } from "./game-server/UDPServer";
+const server = new UDPServer(23434, null, "type");
 
-console.log(init('Hello', 'world'));
+server.listen(() => {
+  console.log("opened server");
+});
