8aac44c7359ccaab1d9cdaa2daf60cfaff2d186b
Optifun <frdima19@gmail.com>
2021-06-25 16:20:05 +0700

    Add exports for game server types

---
+++ src/game-server/index.ts
@@ -0,0 +1,11 @@
+export { UDPServer } from "./UDPServer";
+export { Router } from "./Router";
+export {
+  IComposite,
+  IHandler,
+  RouteCallback,
+  RouteMap,
+  Server,
+  ShareCallback,
+  SocketInfo,
+} from "./types";
